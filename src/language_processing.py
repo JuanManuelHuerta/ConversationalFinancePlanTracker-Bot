f5e3d002d4f844cd19b479a761be8a7eebc3639a
JuanManuelHuerta <jmhuertany@gmail.com>
2017-11-27 22:47:39 -0500

    Cleaning up a little

--- src/language_processing.py
+++ src/language_processing.py
@@ -1,9 +1,27 @@
 import sys
 
 
+## find_match: takes each key in dictionary and matches it in the string set.  O(n*m)
+## It will stop when a key is found.
+## if keys collide or compete there will be a problem.
+
 
 def find_match(my_dict,utterance):
     for key in my_dict:
         if key in utterance:
             return my_dict[key]
     return None
+
+
+
+
+###   Filler Phrases
+##    Keyword phrases
+##    Parameter phrases
+##    More of a grammar
+##    Construct a tree
+
+##  { "action":None, "object":{"type":None  "details":None}}
+##  Example:
+##  
+
